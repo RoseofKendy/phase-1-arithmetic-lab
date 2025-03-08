de08799c0f5d55094d8dc4cec30671fb54a26c60
Sharon Kendi <sharonkendi35@gmail.com>
2025-03-08 09:38:41 +0300

    Updates repo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let multiply = num1 * num2; // 31 * 2 = 62
 let random = Math.floor(Math.random() * 100) + 1; // Random number > 0
 
 let num3 = 24;
-let num4 = 7;
+let num4 = 5;
 let mod = num3 % num4; // 24 % 5 = 4
 
 let max = Math.max(1, 3, 5, 10, 20)
