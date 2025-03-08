a41577a46f89df6830568b3cdab28bb1b8514edf
Sharon Kendi <sharonkendi35@gmail.com>
2025-03-08 09:31:31 +0300

    Learns about arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // 31 * 2 = 62
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1; // Random number > 0
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4; // 24 % 5 = 4
+
+let max = Math.max(1, 3, 5, 10, 20)
